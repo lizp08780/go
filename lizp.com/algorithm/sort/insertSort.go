ca8e8c270676a5bfb3e4d2ae684fe874cb76c2d3
lizp <1206009720@qq.com>
2020-02-09 16:02:32 +0800

    排序

---
+++ lizp.com/algorithm/sort/insertSort.go
@@ -0,0 +1,13 @@
+package sort
+
+func InsertSort(arr []int) {
+	for out := 1; out < len(arr); out++ {
+		temp := arr[out]
+		in := out
+
+		for ; in > 0 && arr[in-1] >= temp; in-- {
+			arr[in] = arr[in-1]
+		}
+		arr[in] = temp
+	}
+}
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	a := []int{1, 2, 0, 4, 3, 3}
 	//sort.HeapSort(a)
 	//sort.BubbleSort2
+	sort.InsertSort(a)
 	fmt.Println(a)
 	b := sort.QuickSort(a)
 	fmt.Println(b)
