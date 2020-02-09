c1a25dab7072f1dfa4928c2cc38b40d3502a8938
lizp <1206009720@qq.com>
2020-02-09 15:07:15 +0800

    test

---
+++ lizp.com/algorithm/sort/heapSort.go
@@ -0,0 +1,31 @@
+package sort
+
+func sortMax(a []int, length int) {
+	if length <= 1 {
+		return
+	}
+	depth := length/2 - 1
+	for i := depth; i >= 0; i-- {
+		left := 2*i + 1
+		right := 2*i + 2
+		maxIndex := i
+		if left < length && a[left] > a[maxIndex] {
+			maxIndex = left
+		}
+		if right < length && a[right] > a[maxIndex] {
+			maxIndex = right
+		}
+		if i != maxIndex {
+			a[i], a[maxIndex] = a[maxIndex], a[i]
+		}
+	}
+
+}
+
+func HeapSort(a []int) {
+	length := len(a)
+	for i := length; i > 0; i-- {
+		sortMax(a, i)
+		a[0], a[i-1] = a[i-1], a[0]
+	}
+}
